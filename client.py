3336f961201a2f4a1f4ccb39852ae5a43189c8ab
Lucas Tesser <lucastesser39@hotmail.com>
2024-10-03 17:34:20 -0300

    Removi os comentários do código

--- client.py
+++ client.py
@@ -2,30 +2,26 @@ import socket
 
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("127.0.0.1", 5555))  # Conecta-se ao servidor no localhost, porta 5555
+    client.connect(("127.0.0.1", 5555))  
 
     try:
         while True:
-            # Recebe a mensagem do servidor (pode ser opções ou resultado)
             server_message = client.recv(4096).decode()
             if not server_message:
                 print("Conexão fechada pelo servidor.")
-                break  # Sai do loop se o servidor fechar a conexão
+                break 
             print(server_message)
 
-            # Verifica se o servidor está solicitando uma entrada
             if "Escolha uma opção de filme para votar" in server_message:
                 vote = input()
 
-                # Se o cliente quiser sair
                 if vote == '0':
                     client.sendall(vote.encode())
                     print("Desconectando do servidor...")
-                    break  # Sai do loop para encerrar a conexão
+                    break  
                 else:
                     client.sendall(vote.encode())
             else:
-                # Se a mensagem não solicitar entrada, continuamos recebendo
                 continue
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import socket
 import threading
 
-# Dicionário para armazenar as opções de filmes com suas respectivas contagens de votos
 movies_series_votes = {
     1: {"nome": "Harry Potter", "votos": 0},
     2: {"nome": "Madagascar", "votos": 0},
@@ -14,16 +13,13 @@ movies_series_votes = {
 # Bloqueio para evitar condições de corrida ao atualizar os votos
 lock = threading.Lock()
 
-# Função para lidar com os clientes
 def handle_client(client_socket):
     try:
-        # Envia a mensagem de boas-vindas
         client_socket.sendall("---------------------------------------------------".encode())
         client_socket.sendall("\033[31m""Bem-vindo ao sistema de votação de Filmes/Séries!\n""\033[0m".encode())
         client_socket.sendall("---------------------------------------------------".encode())
 
         while True:
-            # Envia as opções de votação para o cliente
             options_message = "\nOpções disponíveis para votar:\n\n"
             for key, value in movies_series_votes.items(): 
                 options_message += f"{key} - {value['nome']}\n" 
@@ -33,23 +29,18 @@ def handle_client(client_socket):
             options_message += "=========================================\n"
             client_socket.sendall(options_message.encode())
 
-            # Recebe a escolha do cliente
             vote = client_socket.recv(1024).decode().strip()
             if not vote:
                 print("Cliente desconectado.")
-                break  # Sai do loop se o cliente desconectar
+                break  
 
-            # Verifica se a entrada é um número válido
             if vote.isdigit():
                 vote = int(vote)
-
                 if vote == 0:
                     print("Cliente solicitou desconexão.")
                     client_socket.sendall("Conexão encerrada. Obrigado por participar!\n".encode())
-                    break  # Sai do loop e encerra a conexão
-
+                    break  
                 elif vote == 7:
-                    # Envia o resultado da votação para o cliente
                     result_message = "\n""\033[30;47m" "Resultado da votação atual: ""\033[0m" "\n"
                     result_message += "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                     with lock:
@@ -57,7 +48,6 @@ def handle_client(client_socket):
                             result_message += f"{value['nome']}: {value['votos']} votos\n"
                     client_socket.sendall(result_message.encode())
                 elif vote in movies_series_votes:
-                    # Se o voto for válido, atualiza a contagem de votos
                     with lock:
                         movies_series_votes[vote]['votos'] += 1
                     client_socket.sendall(f"Obrigado por votar no filme {movies_series_votes[vote]['nome']}!\n".encode())
@@ -74,8 +64,8 @@ def handle_client(client_socket):
 # Função principal do servidor
 def start_server(): 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("127.0.0.1", 5555))  # Escuta no localhost, porta 5555
-    server.listen(5)  # Permite até 5 conexões simultâneas
+    server.bind(("127.0.0.1", 5555)) 
+    server.listen(5)  
     print("Servidor iniciado. Aguardando conexões...")
 
     while True:
